0eaabec2f35bfd0bb07390892f33ed2d4f4e24e6
rino2000 <rino.rh35@gmail.com>
2026-01-24 21:36:07 +0100

    2 players can join rooms

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+import random
 from enum import Enum
 from typing import List, Tuple, TypedDict
 
 import numpy as np
-from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "secret!"
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", sync_mode="threading")
 
 x_legende: List[int] = list(range(10))
 y_legende: List[chr] = list(map(chr, range(ord("a"), ord("l"))))
@@ -34,16 +35,18 @@ soldaten_2 = ["👆🏿"] * 1
 town_1 = "🏠"
 town_2 = "🏡"
 
+rooms: dict[str, dict] = {}
+
 
 class Spieler(Enum):
     WHITE = 0
     BLACK = 1
 
 
-type Field = List[List[np.array]]
-type Koordiante = Tuple[int, int]
-type Koordianten = List[Koordiante]
-type TownKordinaten = Tuple[int, int]
+Field = List[List[np.array]]
+Koordiante = Tuple[int, int]
+Koordianten = List[Koordiante]
+TownKordinaten = Tuple[int, int]
 
 fields: Field = np.zeros((11, 11)).astype(np.int8).tolist()
 
@@ -52,19 +55,6 @@ class Soldaten(TypedDict):
     spieler: Spieler
     coordinate: Koordianten
     town: TownKordinaten
-
-
-position: Soldaten = {
-    "spieler": Spieler.WHITE,
-    "coordinate": [(3, 4), (4, 3), (4, 4), (4, 5), (4, 6)],
-    "town": (3, 3),
-}
-
-position2: Soldaten = {
-    "spieler": Spieler.BLACK,
-    "coordinate": [(5, 4), (5, 3), (5, 4), (5, 5), (5, 6)],
-    # "town": (6, 6),
-}
 
 
 def set_soldaten(soldaten: Soldaten, field: Field):
@@ -77,7 +67,7 @@ def set_soldaten(soldaten: Soldaten, field: Field):
 
 
 def place_town(spieler: Spieler, coordinated: TownKordinaten, field: Field) -> Field:
-    if coordinated == None:
+    if coordinated is None:
         return
     x, y = coordinated
     field[x][y] = town_1 if spieler == Spieler.WHITE else town_2
@@ -90,18 +80,6 @@ def place_soldaten(spieler: Spieler, coordante: Koordiante, field: Field) -> Fie
     return field
 
 
-def initialize_field() -> Field:
-    for field in range(len(fields) - 1):
-        fields[field][0] = x_legende[field]
-
-    fields[-1] = y_legende
-
-    set_soldaten(soldaten=position, field=fields)
-    set_soldaten(soldaten=position2, field=fields)
-
-    return fields
-
-
 def check_town_count(fields: Field) -> bool:
     t1, t2 = np.sum(fields == town_1), np.sum(fields == town_2)
     return True if t1 == 1 and t2 == 1 else False
@@ -109,16 +87,51 @@ def check_town_count(fields: Field) -> bool:
 
 @app.route("/")
 def hello_world():
-    fields = initialize_field()
-    np_field = np.array(fields, dtype=object)
-    print(f"{'ja' if check_town_count(np_field) else 'nein'}")
     return render_template("view.html", data=fields)
 
 
-@app.post("/start")
-def start():
-    positions = request.json
-    return jsonify(positions)
+@socketio.on("join_room")
+def join(data):
+    room = data["room"]
+    sid = request.sid  # Session ID
+
+    # Create room if it doesn't exist
+    if room not in rooms:
+        rooms[room] = {
+            "field": np.zeros((11, 11)).astype(np.uint8).tolist(),
+            "players": [],
+            "colors": {},
+        }
+
+    players = rooms[room]["players"]
+
+    # Check if room has space
+    if len(players) >= 2:
+        emit("error", {"message": "Room full!"})
+        return
+
+    # add player to room
+    players.append(sid)
+
+    # if first player in a room set color
+    if len(players) == 1:
+        starter = random.choice(list(Spieler))
+        rooms[room]["colors"].update({sid: starter.value})
+
+    # check if one player is in room then set the other player the opposite color
+    if len(players) == 2:
+        player1_color = rooms[room]["colors"]
+        rooms[room]["colors"].update({sid: 0 if player1_color == 1 else 0})
+
+        join_room(room)
+        emit("joined_room", {"room": room})
+        print(f"Player {sid} joined room {room}")
+        print(rooms)
+    else:
+        join_room(room)
+        emit("joined_room", {"room": room, "player": sid})
+        print(f"Player {sid} joined room {room}")
+        print(rooms)
 
 
 @socketio.on("place_soldaten")
@@ -134,4 +147,5 @@ def handle_place_soldaten(data):
 
 
 if __name__ == "__main__":
-    socketio.run(app)
+    # socketio.run(app, debug=True)
+    socketio.run(app, host="127.0.0.1", port=8000, debug=True)
